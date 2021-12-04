02f60f0650d0291d42a7c2145be3a6aba198e906
Andrey Chumakov <and.wow87@gmail.com>
2021-12-04 04:05:52 +0300

    adding data along with page load

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,10 @@ const battons = document.querySelector('.battons'),
       display = document.querySelector('.display'),
       calc = document.querySelector('.calc');
 
+const date = new Date(),
+      todaysdata = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
 
+      
 const prise = {
     hard: 310,
     bonus: 265,
@@ -24,21 +27,56 @@ const dataB = {
     Sum: 0,
     Earnings: 0,
 };
-const date = new Date();
-const todaysdata = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
-updateDataB();
-function updateDataB() {
-    if(localStorage.getItem(todaysdata)){
-        
-        const dataA = JSON.parse(localStorage.getItem(todaysdata));
-        for(let key in dataA){
-            dataB[key] = dataA[key];
-        }
-        console.log(dataB);
-    }
-}
 
 let dots = [];
+
+function updateDataB() {
+
+    if(localStorage.getItem(todaysdata)){       //  checks is today's date or not
+        
+        const dataA = JSON.parse(localStorage.getItem(todaysdata));
+        for(let key in dataA){         //   update DataB
+            dataB[key] = dataA[key];
+        }
+        
+    }
+    
+}
+
+
+function createDisplay() {
+    const display = document.createElement('div');
+    display.classList.add('display');
+    calc.append(display); 
+
+    for(let key in dataB){
+        const strng = document.createElement('div');
+        strng.classList.add('window-left');
+        strng.setAttribute('name', key);
+        display.append(strng);
+        dots.push(strng);
+    }
+
+    batton.forEach(btn => {
+        const name =  btn.dataset.name;
+            for(let key in dataB){
+                    
+                if( key == name) {
+                    
+                        dots.forEach(item => {
+                            if(item.getAttribute('name') == name){
+                                    item.innerHTML = `<h1>${key}: ${dataB[key]}</h1> `;
+                                    item.classList.add('active');
+                                    
+                            }
+                        });
+                }
+            }
+            allData();
+    });
+}
+    
+
 function allData() {
     
     dataB.All = dataB.Hard + dataB.Bonus + dataB.Normal + dataB.Easy + dataB.Person;
@@ -48,7 +86,7 @@ function allData() {
     dataB.Earnings = (dataB.Sum * 25) / 100;
 
     const All = document.querySelector('[name=All]');
-    All.innerHTML = `<h1>Всео кальянов: ${dataB.All}</h1> `;
+    All.innerHTML = `<h1>количество кальянов: ${dataB.All}</h1> `;
     All.classList.add('active');
 
     const Sum = document.querySelector('[name=Sum]');
@@ -56,35 +94,12 @@ function allData() {
     Sum.classList.add('active');
 
     const Earnings = document.querySelector('[name=Earnings]');
-    Earnings.innerHTML = `<h1>зароботок: ${dataB.Earnings}</h1> `;
+    Earnings.innerHTML = `<h1>дневной процент: ${dataB.Earnings}</h1> `;
     Earnings.classList.add('active');
 
     localStorage.setItem(todaysdata, JSON.stringify(dataB));
 }
 
-
-
-function createDisplay() {
-    const display = document.createElement('div');
-    display.classList.add('display');
-    calc.append(display); 
-
-    for(let key in dataB){
-
-        const strng = document.createElement('div');
-        strng.classList.add('window-left');
-        strng.setAttribute('name', key);
-        display.append(strng);
-        dots.push(strng);
-
-        } 
-        
-        
-    }
-    
-
-
-createDisplay();
 
 batton.forEach(btn => {
     btn.addEventListener('click', (e) => {
@@ -97,9 +112,6 @@ batton.forEach(btn => {
 
                     dataB[key]++;
                     allData();
-                    console.log(dataB.Sum);
-                    console.log(dataB.Earnings);
-
                     dots.forEach(item => {
                         if(item.getAttribute('name') == name){
                                 item.innerHTML = `<h1>${key}: ${dataB[key]}</h1> `;
@@ -115,7 +127,9 @@ batton.forEach(btn => {
         
     });
 });
-document.querySelectorAll('.window-left').forEach(item => {
+
+
+document.querySelectorAll('.window-left').forEach(item => {     
 
     item.addEventListener('click', event=> {
        const name = item.getAttribute('name');
@@ -127,16 +141,21 @@ document.querySelectorAll('.window-left').forEach(item => {
         dataB[name] = 0;
         item.innerHTML = `<h1>${name}: ${dataB[name]}</h1> `;
     }
-    allData();
-
-       
+    allData();  
     });
    
 });
 
 
 
-    
+updateDataB(); 
+createDisplay();
+ 
+
+
+
+
+
 
     
     
@@ -146,60 +165,6 @@ document.querySelectorAll('.window-left').forEach(item => {
 
  
 
-// <div class="window-left" data-name="Hard">
-// <div class="delete"></div>
-
-
-// batton[0].addEventListener('click', (e) => {
-//     dataB.Hard++;
-//     allData();
-
-//     windowLeft[0].innerHTML = `<h1>Hard: ${dataB.Hard}</h1> `;
-//     windowLeft[0].
-
-    
-    
-// });
-
-// batton[1].addEventListener('click', (e) => {
-//     dataB.Bonus++;
-//     allData();
-//     windowLeft[1].innerHTML = `<h1>Bonus: ${dataB.Bonus}</h1>`;
-//     windowLeft[1].classList.add('active');
-// });
-
-// batton[2].addEventListener('click', (e) => {
-//     dataB.Normal++;
-//     allData();
-//     windowLeft[2].innerHTML = `<h1>Normal: ${dataB.Normal}</h1>`;
-//     windowLeft[2].classList.add('active');
-// });
-
-// batton[3].addEventListener('click', (e) => {
-//     dataB.Easy++;
-//     allData();
-//     windowLeft[3].innerHTML = `<h1>Easy: ${dataB.Easy}</h1>`;
-//     windowLeft[3].classList.add('active');
-
-// });
-
-// batton[4].addEventListener('click', (e) => {
-//     dataB.Person++;
-//     allData();
-//     windowLeft[4].innerHTML = `<h1>Person: ${dataB.Person}</h1>`;
-//     windowLeft[4].classList.add('active');
-    
-// });
-
-// batton.forEach((i)=> {
-//     i.addEventListener('click', (e) => {
-    
-//         allData();
-//         windowRight.innerHTML = `<h1>All: ${dataB.All}</h1>`;
-//         windowRight.classList.add('active');
-        
-//     });
-// })
 
 
 
